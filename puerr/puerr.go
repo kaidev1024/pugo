2c22eef621777819067a2c977e9ccc3015acf198
Xiaokai Li <kaidev1024@gmail.com>
2026-03-14 21:08:48 -0700

    rename LeaderboardTypeMismatch

--- puerr/puerr.go
+++ puerr/puerr.go
@@ -4,7 +4,7 @@ import "fmt"
 
 var InterestsMismatch = fmt.Errorf("interests do not match")
 var PlayerTypeMismatch = fmt.Errorf("player type does not match")
-var ScoringTypeMismatch = fmt.Errorf("scoring type does not match")
+var LeaderboardTypeMismatch = fmt.Errorf("scoring type does not match")
 var GameTypeMismatch = fmt.Errorf("game type does not match")
 var MustBeUoi = fmt.Errorf("ID must start with 'uoi'")
 var MustBeTeam = fmt.Errorf("ID must start with 'tms'")
