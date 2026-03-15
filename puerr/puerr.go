abcc7c23e37c6bc5bfdaff980b3c9f4b24d314a7
Xiaokai Li <kaidev1024@gmail.com>
2026-03-14 21:04:37 -0700

    minor fix

--- puerr/puerr.go
+++ puerr/puerr.go
@@ -2,10 +2,10 @@ package puerr
 
 import "fmt"
 
-var InterestsDoNotMatch = fmt.Errorf("interests do not match")
-var PlayerTypeDoNotMatch = fmt.Errorf("player type does not match")
-var ScoringTypeDoNotMatch = fmt.Errorf("scoring type does not match")
-var GameTypeDoNotMatch = fmt.Errorf("game type does not match")
+var InterestsMismatch = fmt.Errorf("interests do not match")
+var PlayerTypeMismatch = fmt.Errorf("player type does not match")
+var ScoringTypeMismatch = fmt.Errorf("scoring type does not match")
+var GameTypeMismatch = fmt.Errorf("game type does not match")
 var MustBeUoi = fmt.Errorf("ID must start with 'uoi'")
 var MustBeTeam = fmt.Errorf("ID must start with 'tms'")
 var MustBeLeaderboard = fmt.Errorf("ID must start with 'ldb'")
