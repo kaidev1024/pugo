c2c614888003b785329b12eb80a9bde8d36803cd
Xiaokai Li <kaidev1024@gmail.com>
2026-03-17 19:26:32 -0700

    add TrimScore

--- util.go
+++ util.go
@@ -1,5 +1,14 @@
 package pugo
 
+import (
+	"fmt"
+	"strings"
+)
+
 func GetFullName(firstName, lastName string) string {
 	return firstName + " " + lastName
+}
+
+func TrimScore(score float32) string {
+	return strings.TrimRight(strings.TrimRight(fmt.Sprintf("%.2f", score), "0"), ".")
 }
